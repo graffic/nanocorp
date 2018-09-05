307357b5c1d017241cfd40c4aa2b4c413e1a2166
Javier Gonel <bolibic@gmail.com>
2018-09-05 15:46:33 +0300

    fix(deploy): path for kubectl

--- deploy/travis-deploy.sh
+++ deploy/travis-deploy.sh
@@ -6,7 +6,9 @@ set -o pipefail
 set -o errexit
 set -o nounset
 
-cd "$(cd "$(dirname "$0")"; pwd)"
+DEPLOY_DIR="$(cd "$(dirname "$0")"; pwd)"
+PATH=$DEPLOY_DIR:$PATH
+cd $DEPLOY_DIR
 
 echo "Prepare secrets..."
 openssl aes-256-cbc -K $encrypted_4e81e2b6d12b_key -iv $encrypted_4e81e2b6d12b_iv -in secrets.tar.enc -out secrets.tar -d
@@ -15,7 +17,6 @@ tar xvf secrets.tar
 echo "Install kubectl..."
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
-alias kubectl=./kubectl
 
 echo "Configure kubectl..."
 kubectl config set-cluster k8s-cluster --embed-certs=true --server=${GKE_ENDPOINT} --certificate-authority=ca.crt
