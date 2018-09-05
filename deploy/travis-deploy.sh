e15fd00b578e9debceff3a920c383fee9f130d6d
Javier Gonel <bolibic@gmail.com>
2018-09-05 15:04:14 +0300

    fix(build): debug deploy script

--- deploy/travis-deploy.sh
+++ deploy/travis-deploy.sh
@@ -9,13 +9,17 @@ set -o nounset
 cd "$(cd "$(dirname "$0")"; pwd)"
 
 # Prepare k8s keys
+echo "Prepare GKE user token..."
 openssl aes-256-cbc -K $encrypted_d6c310cbd15d_key -iv $encrypted_d6c310cbd15d_iv -in gke_user_token.enc -out gke_user_token -d
+echo "Prepare GKE certificate..."
 openssl aes-256-cbc -K $encrypted_d6c310cbd15d_key -iv $encrypted_d6c310cbd15d_iv -in ca.crt.enc -d | base64 --decode > ca.crt
 
+echo "Configure kubectl..."
 kubectl config set-cluster k8s-cluster --embed-certs=true --server=${GKE_ENDPOINT} --certificate-authority=ca.crt
 kubectl config set-credentials travis-echo --token=$(cat gke_user_token)
 kubectl config set-context travis --cluster=k8s-cluster --user=travis-deploy
 kubectl config use-context travis
 kubectl config current-context
 
+echo "Deploy..."
 kubectl apply -f config.yml
